fb49855ec552e8f0800acce5d4b156898e67d8bd
Rafael T. Sanchez Bailo <r.sanchezbailo@gmail.com>
2017-10-12 19:01:31 +0100

    implicit notation

--- Exercises/Chapter3.lean
+++ Exercises/Chapter3.lean
@@ -5,20 +5,38 @@ variables p q r s : Prop
 -- commutativity of ∧ and ∨
 lemma L1 : p ∧ q → q ∧ p := assume h : p ∧ q, and.intro (and.right h) (and.left h)
 
-lemma L2 : p ∧ q ↔ q ∧ p := iff.intro
-(assume h : p ∧ q, L1 p q h)
-(assume h : q ∧ p, L1 q p h)
+lemma L2 : p ∧ q ↔ q ∧ p := iff.intro (L1 p q) (L1 q p)
 
-lemma L3 : p ∨ q → q ∨ p := assume h : p ∨ q, or.elim h
-(assume hp : p, or.intro_right q hp)
-(assume hq : q, or.intro_left p hq)
+lemma L3 : p ∨ q → q ∨ p := assume h : p ∨ q, or.elim h (or.intro_right q) (or.intro_left p)
 
-lemma L4 : p ∨ q ↔ q ∨ p := iff.intro
-(assume h : p ∨ q, L3 p q h)
-(assume h : q ∨ p, L3 q p h)
+lemma L4 : p ∨ q ↔ q ∨ p := iff.intro (L3 p q) (L3 q p)
 
 -- -- associativity of ∧ and ∨
--- example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+lemma L5 : (p ∧ q) ∧ r → p ∧ (q ∧ r) := assume h : (p ∧ q) ∧ r,
+have hpq : (p ∧ q), from and.left h,
+have hp : p, from and.left hpq,
+have hq : q, from and.right hpq,
+have hr : r, from and.right h,
+and.intro (hp)  (and.intro (hq) (hr))
+
+lemma L6 : p ∧ (q ∧ r) → (p ∧ q) ∧ r := assume h : p ∧ (q ∧ r),
+have hp : p, from and.left h,
+have hqr : (q ∧ r), from and.right h,
+have hq : q, from and.left hqr,
+have hr : r, from and.right hqr,
+and.intro (and.intro (hp) (hq)) (hr)
+
+lemma L7 : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := iff.intro (L5 p q r) (L6 p q r)
+
+lemma L8 : (p ∨ q) ∨ r → p ∨ (q ∨ r) := sorry
+
+lemma L9 : p ∨ (q ∨ r) → (p ∨ q) ∨ r := sorry
+
+lemma L10 : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := iff.intro
+(L8 p q r)
+(L9 p q r)
+
+
 -- example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
 
 -- -- distributivity
